216c227695f8064e126c04519804e3fa003c4a01
sophia4719 <isabelsophia212@gmail.com>
2025-11-13 18:25:45 +0000

    Sophia isabel de souza Oliveira  Modulo 2  tarde

--- app_flask.py
+++ app_flask.py
@@ -7,7 +7,36 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+@app.route("/soma")
+def somar():
+    valor1 = request.args.get ("valor1", type=int)
+    valor2 = request.args.get ("valor2", type=int)
+    total = valor1 + valor2
+    return {"resultado":total}
+
+@app.route("/subtracao")
+def subtrair():
+    valor1 = request.args.get ("valor1", type=int)
+    valor2 = request.args.get ("valor2", type=int)
+    total = valor1 - valor2
+    return {"resultado":total }
+
+@app.route("/multiplicaçao")
+def multiplicar():
+    valor1 = request.args.get ("valor1", type=int)
+    valor2 = request.args.get ("valor2", type=int)
+    total = valor1 * valor2
+    return {"resultado":total }
+
+@app.route("/divisao")
+def dividir():
+    valor1 = request.args.get ("valor1", type=int)
+    valor2 = request.args.get ("valor2", type=int)
+    if valor2 == 0:
+        return "Erro de divasao por 0"
+    total = valor1 / valor2
+    return {"resultado":total }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
