8718958ef434582a26b1ba3265eff726db00cd5b
Seishiro Nagi <pito987bl@gmail.com>
2025-11-18 14:07:04 +0000

    Matheus Gabriel Ferreira da Silva

--- app_flask.py
+++ app_flask.py
@@ -2,12 +2,39 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+@app.route('/soma', methods=['GET'])
+def soma():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    resultado = a + b
+    return {'resultado': resultado}
 
-if __name__ == "__main__":
+@app.route('/subtrai', methods=['GET'])
+def subtrai():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    resultado = a - b
+    return {'resultado': resultado}
+
+@app.route('/divide', methods=['GET'])
+def divide():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 1))
+    if b == 0:
+        return {'erro': 'Divisão por zero não permitida!'}
+    resultado = a / b
+    return {'resultado': resultado}
+
+@app.route('/multiplica', methods=['GET'])
+def multiplica():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    resultado = a * b
+    return{'resultado': resultado}
+
+if __name__ == "___main__":
     app.run(debug=True)
