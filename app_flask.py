d28da81ff61f71a3865ea840e64e77b80d6abb8f
Eduardo11969 <eduardocontato1208@gmail.com>
2025-11-18 13:54:48 +0000

    EDUARDO OLIVEIRA SILVA MARTINS MODULO 2 MANHÃ

--- app_flask.py
+++ app_flask.py
@@ -7,7 +7,39 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+@app.route("/soma")
+def somar():
+    numero1=  request.args.get("valor1", type=int)
+    numero2=  request.args.get("valor2", type=int)
+    total = numero1 + numero2
+    return {"operação": "soma", "resultado": total}
+
+@app.route("/subtrair")
+def subtrair():
+    numero1= request.args.get("valor1", type=int)
+    numero2= request.args.get("valor2", type=int)
+    total = numero1 - numero2
+    return {"operação": "subtrair", "resultado":f"{numero1} + {numero2} = {total}"}
+
+
+@app.route("/multiplicar")   
+def multiplicar():
+    numero1= request.args.get("valor1", type=int)
+    numero2= request.args.get("valor2", type=int)
+    total = numero1 * numero2
+    return {"operação": "multiplicar", "resultado":f"{numero1} * {numero2} = {total}"}
+
+
+@app.route("/divisao")    
+def divisao():
+    numero1= request.args.get("valor1", type=int)
+    numero2= request.args.get("valor2", type=int)
+    if numero2 == 0:
+        return {"erro": "Divisão por zero não é permitida."}, 400
+    total = numero1 / numero2
+    return {"operação": "divisao", "resultado":f"{numero1} / {numero2} = {total}"}
+          
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
